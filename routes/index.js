5700e963c943160627676e43e26dec33259ad8b6
igoryen <igor.yentaltsev@gmail.com>
2014-12-23 13:03:53 -0500

    Add the hello world function

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' }); // 9
 });
 
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' }) // 14
+});
+
 module.exports = router; // 10
